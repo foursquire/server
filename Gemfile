284eebc5746eb9b58905da1c0c7f024c3cca06d2
Tim Anglade <timanglade@gmail.com>
2012-06-23 16:07:25 -0700

    Extract latitute and longitude from checkins

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
 gem 'sinatra', '1.3.2'
 gem 'thin'
+gem 'json'
--- foursquire-server.rb
+++ foursquire-server.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'open-uri'
+require 'json/ext'
 
 get '/' do
   "Hello, world"
@@ -6,7 +8,8 @@ end
 
 post '/checkin' do
 	# Handle Foursquare checkins as they arrive
+
 	request.body.rewind
-	logger.info request.media_type
-	logger.info request.body.read
+	checkin = JSON.parse URI.decode request.body.read.split('checkin=').last.split('&user').first
+	logger.info checkin["venue"]["location"]["lat"].to_s + ' / ' + checkin["venue"]["location"]["lng"].to_s
 end
