ec40c7718e5bb7409e563ed76a350635fe64d559
Tim Anglade <timanglade@gmail.com>
2012-06-23 17:16:22 -0700

    First pass at integrating with Foursquare

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 gem 'sinatra', '1.3.2'
 gem 'thin'
 gem 'json'
+gem 'httparty'
--- foursquire-server.rb
+++ foursquire-server.rb
@@ -1,9 +1,47 @@
 require 'sinatra'
 require 'open-uri'
 require 'json/ext'
+require 'httparty'
 
 get '/' do
   "Hello, world"
+end
+
+class Usergrid
+  include HTTParty
+  base_uri 'https://api.usergrid.com/Foursquire/foursquire'
+  debug_output $stderr
+  #default_params :output => 'json'
+  format :json
+  headers "Content-Type" => "application/json"
+end
+
+get '/login/:token' do
+	# Receive a token as a query string 
+	token = params[:token]
+
+	# Call foursquare
+	fq_response = HTTParty.get "https://api.foursquare.com/v2/users/self?oauth_token=#{token}&v=20120623"
+
+	# Receive the JSON, store the request.body => response.user.id
+	fq_user = fq_response.parsed_response["response"]["user"]; fq_id = fq_user["id"]
+
+	# Query Usergrid /users/?ql=fq.id%3D=thatID
+	ug_response = Usergrid.get '/users', :query => { 'ql' => "fq.id='#{fq_id}'" } 
+
+	logger.info ug_response
+	if ug_response.parsed_response["entities"].empty?
+		# If no results
+		logger.info "that user doesn't exist yet"
+		Usergrid.delete "/users/fq_#{fq_id}"
+		response = Usergrid.post '/users', :body => { 'username' => "fq_#{fq_id}", 'email' => fq_user["contact"]["email"], 'fq' => fq_user }.to_json
+		#POST /users { username: fq_thatID, fq: the whole response.user, email: response.user.contact.email}
+	else
+		# If results
+		logger.info "Found a user!"
+		#PUT /users/UUID I received from the query above { fq: the whole response.user }
+		response = Usergrid.put "/users/fq_#{fq_id}", :body => { 'fq' => fq_user }.to_json
+	end
 end
 
 post '/checkin' do
@@ -12,4 +50,7 @@ post '/checkin' do
 	request.body.rewind
 	checkin = JSON.parse URI.decode request.body.read.split('checkin=').last.split('&user').first
 	logger.info checkin["venue"]["location"]["lat"].to_s + ' / ' + checkin["venue"]["location"]["lng"].to_s
+
+	Usergrid.put "/users/fq_#{fq_id}", :body => { 'location' => { 'latitude' => checkin["venue"]["location"]["lat"], 'longitude' => checkin["venue"]["location"]["lng"] } }.to_json
+	#PUT /users?ql=fq.id=thatID {location:{latitute: lat , longitude: lng}}
 end
